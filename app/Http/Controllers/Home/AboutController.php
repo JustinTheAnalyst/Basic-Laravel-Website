896a4078bd83c7445471abb02cf3a0fbedeaa5de
Justin <justin.tys@hotmail.com>
2023-02-08 13:33:06 +1300

    finish update about page details to db

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -17,4 +17,46 @@ class AboutController extends Controller
         // passing compact method into about page
         return view('admin.about_page.about_page_all', compact('aboutpage'));
     }
+
+    public function UpdateAbout(Request $request){
+        $about_id  = $request->id;
+
+        if($request->file('about_image')){
+            $image = $request->file('about_image');
+            $new_file_name = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+
+            Image::make($image)->resize(523,605)->save('upload/home_about/'.$new_file_name);
+
+            $save_url = 'upload/home_about/'.$new_file_name;
+
+            About::findOrFail($about_id)->update([
+                'title' => $request->title,
+                'short_title' => $request->short_title,
+                'short_description' => trim($request->short_description),
+                'long_description' => $request->long_description,
+                'about_image' => $save_url,
+            ]);
+
+            $notification = array(
+                'message' => 'About page is updated successfully with image', 
+                'alert-type' => 'success'
+            );
+
+            return redirect()->back()->with($notification);
+        } else {
+            About::findOrFail($about_id)->update([
+                'title' => $request->title,
+                'short_title' => $request->short_title,
+                'short_description' => trim($request->short_description),
+                'long_description' => $request->long_description,
+            ]);
+    
+            $notification = array(
+                'message' => 'About page is updated successfully without image', 
+                'alert-type' => 'success'
+            );
+    
+            return redirect()->back()->with($notification);
+        }
+    }
 }
